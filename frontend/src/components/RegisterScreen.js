5691bfa9ff242ff30f8720b48b24993bf649a6f9
Raj-Ailani <ailaniraj1@gmail.com>
2021-07-01 00:09:25 +0530

    Changes in RegisterScreen

--- frontend/src/components/RegisterScreen.js
+++ frontend/src/components/RegisterScreen.js
@@ -1,6 +1,6 @@
 import React ,{ useState} from 'react'
-import {Link, } from 'react-router-dom'
-import {Form,Button,Row,Col, FormGroup, FormLabel, FormControl, Container} from 'react-bootstrap'
+
+import {Form,Button,Row,Col, FormGroup, FormLabel, FormControl} from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
 
 const RegisterScreen = () => {
