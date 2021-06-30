272b6ce3fd770efe2bf7ebd53b048d98a8c3330b
Raj-Ailani <ailaniraj1@gmail.com>
2021-06-30 09:13:46 +0530

    Added User Backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import userRoutes from './Users/routes.js'
 
 dotenv.config()
+connectDB()
+
 const app= express()
 app.use(express.json());
+
+app.use('/api/users',userRoutes)
 
 app.use(express.json())
 app.get('/', (req,res)=>{
