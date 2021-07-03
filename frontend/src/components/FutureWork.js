69c396fe4ee5f79fbb8a51675797246814454ff4
Raj-Ailani <ailaniraj1@gmail.com>
2021-07-03 23:37:45 +0530

    Added Future Work

--- frontend/src/components/FutureWork.js
+++ frontend/src/components/FutureWork.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container,Row } from 'react-bootstrap'
 const FutureWork = () => {
     return (
         <div>
         <Container id='future-work'>
       <h1>Future Work</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed leo neque, fermentum euismod enim ut, iaculis pretium ipsum. Nam a pulvinar nulla, ac posuere mauris. Nullam nibh libero, dapibus vestibulum augue eget, vulputate ultrices arcu. Donec commodo viverra urna ac blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris ut diam nec nisl aliquet lobortis ut scelerisque velit. In hac habitasse platea dictumst. In suscipit nisi sit amet nisi facilisis suscipit. Cras at tellus ligula. Ut varius turpis eget lacus vestibulum, a blandit lectus varius. Vestibulum cursus eget velit nec convallis. Etiam consequat hendrerit nisl at finibus. Donec dignissim sapien quis elementum tincidunt. Duis at accumsan dui. Vivamus fermentum eros sollicitudin, fermentum urna vel, finibus sapien. Morbi mattis enim tortor, ut molestie neque facilisis a.
-      <br></br>
-     <p> Suspendisse rutrum erat a iaculis eleifend. Morbi molestie sollicitudin porttitor. Nunc eget orci id felis tristique sodales. Phasellus a tortor mi. Aliquam iaculis non lacus id consectetur. Curabitur sit amet ullamcorper arcu. Ut accumsan consectetur lorem vel bibendum. Integer placerat pretium placerat. Mauris lacinia ultricies massa, id condimentum ex sollicitudin vel. Donec malesuada lacinia augue, quis congue justo fringilla vel. Sed pellentesque eros vitae neque mattis congue. Donec euismod ligula id congue sagittis. Nullam ac maximus orci. In tincidunt ultrices lectus, vel ornare augue varius sit amet. Duis ut ex iaculis, luctus massa eget, fermentum magna. Mauris tincidunt dui sed mauris imperdiet, eget scelerisque ante tincidunt.</p>
-      <br></br>
-      Mauris consectetur dignissim nisl sit amet viverra. Maecenas semper feugiat eros, nec ullamcorper dolor efficitur vel. Maecenas pellentesque eget turpis eget interdum. Nullam vestibulum dui eu velit rutrum feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ex odio, varius in aliquam in, gravida quis est. Ut sed eros nisi. Duis quis justo at turpis ultrices fringilla eu sit amet libero. Pellentesque non auctor mauris.</p>
+      <h3>Solar Power Forecasting :- </h3>
+     <p> It is the process of gathering and analyzing data in order to predict solar power
+        generation on various time horizons with the goal to mitigate the impact of solar intermittency. Solar
+        power forecasts are used for efficient management of the electric grid and for power trading.
+        As major barriers to solar energy implementation, such as materials cost and low conversion efficiency,
+        continue to fall, issues of intermittency and reliability have come to the fore. The intermittency issue has
+        been successfully addressed and mitigated by solar forecasting in many cases.</p>
+      <Container>
+        <Row>
+         
+          <img alt='fullPlot' src = '/assets/FullPlot.jpg'></img>
+         
+        </Row>
+        <Row>
+         
+          <img alt='fullPlot' src = '/assets/24hPlot.jpg'></img>
+         
+        </Row>
+      </Container>
         </Container>
         <Container id='arrow'>
           <a href='#about-us'><p>Next <img src='/assets/arrow.png' alt='arrow'></img></p></a>
